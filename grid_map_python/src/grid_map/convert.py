5d9893fde1217d4b119f5d0568f42724fdf5dd93
Akash <97721132+Akash-1410@users.noreply.github.com>
2023-08-25 18:35:20 +0900

    Added function to change from nav_msgs/OccupancyGrid to GridMapMsg

--- grid_map_python/src/grid_map/convert.py
+++ grid_map_python/src/grid_map/convert.py
@@ -15,7 +15,7 @@ def from_msg(msg, cls=GridMapBinding):
     msg.info.resolution,
     np.array((msg.info.pose.position.x, msg.info.pose.position.y))
   )
-
+  gm.setBasicLayers(list(msg.basic_layers))
   for i, layer in enumerate(msg.layers):
     gm.add(layer, np.float32(np.reshape(msg.data[i].data, (msg.data[i].layout.dim[1].size, msg.data[i].layout.dim[0].size), order='F')) )
 
@@ -31,6 +31,7 @@ def to_msg(self):
   msg.info.pose.position.x, msg.info.pose.position.y = self.getPosition()
   msg.info.pose.orientation.w = 1
   msg.layers = list(self.getLayers())
+  msg.basic_layers=list(self.getBasicLayers())
   for layer in self.getLayers():
     matrix = self[layer]
     data_array = Float32MultiArray()
@@ -40,3 +41,35 @@ def to_msg(self):
     msg.data.append(data_array)
 
   return msg
+  
+#Converts OccupancyGrid Message to GridMapMsg 
+def from_OccupancyGrid(map1,cls=GridMapBinding):
+    msg = GridMapMsg()
+    msg.info.header.stamp=map1.header.stamp
+    msg.info.header.frame_id=map1.header.frame_id
+    msg.info.resolution=map1.info.resolution
+    msg.info.length_x=map1.info.width*map1.info.resolution
+    msg.info.length_y=map1.info.height*map1.info.resolution
+    
+    #Next step is due to difference in center definition
+    #OccupancyGrid defines coordinate (0,0) as the location of map frame in real world. Occupancy grid defines cell [0,0] as the cell with the most negative x and y value
+    #GridMapMsg defines coordinate (0,0) as the center of the grid map generated. Grid Map defines cell [0,0] as the cell with the most positive x and y value
+    msg.info.pose.position.x=0.5*msg.info.length_x+map1.info.origin.position.x 
+    msg.info.pose.position.y=0.5*msg.info.length_y+map1.info.origin.position.y
+    msg.info.pose.position.z=0.0 #Can be any value
+    
+    msg.info.pose.orientation.x=0.0
+    msg.info.pose.orientation.y=0.0
+    msg.info.pose.orientation.z=0.0
+    msg.info.pose.orientation.w=1.0
+    msg.layers=['grid']
+    msg.basic_layers=['grid']
+    data_array=Float32MultiArray()
+    data_array.layout.dim.append(MultiArrayDimension("column_index", map1.info.height, map1.info.height*map1.info.width)) #length_y is coloumn index is height
+    data_array.layout.dim.append(MultiArrayDimension("row_index", map1.info.width, map1.info.width)) #length_x is row index is width
+    data_array.data = map1.data
+    #inverse iteration due to difference in convention of grid_map_msg/GridMapMsg and nav_msgs/OccupancyGrid
+    data_array.data = data_array.data[::-1] 
+    msg.data.append(data_array)
+    
+    return msg
